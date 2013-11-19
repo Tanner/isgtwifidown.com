274172b34e86f32f13e7bdfe0b6b7cbf208769f2
Tanner Smith <tanner@tssoftware.net>
2013-11-19 10:33:10 -0500

    Don't check for new data every request, only ever 5 minutes.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 	"html/template"
 	"log"
+	"time"
 	"github.com/codegangsta/martini"
 	"github.com/tanner/isgtwifidown.com/gtwifi"
 )
@@ -15,25 +16,65 @@ type PageData struct {
 	Reason string
 }
 
+type LastData struct {
+	Status int
+	Reason string
+	TimeRetrieved time.Time
+}
+
+var lastData LastData;
+
 func main() {
 	m := martini.Classic()
 
 	template := template.Must(template.ParseFiles("index.tmpl"))
 
-	m.Get("/", func(res http.ResponseWriter, req *http.Request) {
-		status, err := gtwifi.GetStatus()
-		if err != nil {
-			log.Println(err);
-		}
+	schedule(checkData, 5 * time.Minute)
+	checkData()
 
+	m.Get("/", func(res http.ResponseWriter, req *http.Request) {
 		data := PageData{}
-		data.Green = status.Status == gtwifi.GREEN
-		data.Yellow = status.Status == gtwifi.YELLOW
-		data.Red = status.Status == gtwifi.RED
-		data.Reason = status.Reason
+		data.Green = lastData.Status == gtwifi.GREEN
+		data.Yellow = lastData.Status == gtwifi.YELLOW
+		data.Red = lastData.Status == gtwifi.RED
+		data.Reason = lastData.Reason
 
 		template.Execute(res, data)
 	})
 
 	m.Run()
+}
+
+func checkData() {
+	log.Println("Retrieving new data...");
+
+	status, err := gtwifi.GetStatus()
+
+	if err != nil {
+		log.Println(err);
+		return
+	}
+
+	log.Println("New data retrieved!");
+
+	lastData = LastData{status.Status, status.Reason, time.Now()}
+}
+
+func schedule(caller func(), delay time.Duration) chan bool {
+	ticker := time.NewTicker(delay)
+	stop := make(chan bool)
+
+	go func() {
+		for {
+			select {
+			case <- ticker.C:
+				caller();
+			case <- stop:
+				ticker.Stop()
+				return
+			}
+		}
+	}();
+
+	return stop
 }
