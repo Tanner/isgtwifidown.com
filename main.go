028431083dbc2d79ef76d59726eb36fbcd9a91c2
Tanner Smith <tanner@tssoftware.net>
2013-11-19 09:52:29 -0500

    Add initial page.

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+	"html/template"
+	"log"
+	"github.com/codegangsta/martini"
+	"github.com/tanner/isgtwifidown.com/gtwifi"
+)
+
+type PageData struct {
+	Green bool
+	Yellow bool
+	Red bool
+	Reason string
+}
+
+func main() {
+	m := martini.Classic()
+
+	m.Get("/", func(res http.ResponseWriter, req *http.Request) {
+		status, err := gtwifi.GetStatus()
+		if err != nil {
+			log.Println(err);
+		}
+
+		data := PageData{}
+		data.Green = status.Status == gtwifi.GREEN
+		data.Yellow = status.Status == gtwifi.YELLOW
+		data.Red = status.Status == gtwifi.RED
+		data.Reason = status.Reason
+
+		template := template.Must(template.ParseFiles("index.tmpl"))
+
+		template.Execute(res, data)
+	})
+
+	m.Run()
+}
